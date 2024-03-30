dc5f624256d2e888413ff109edfddb230bd0e63e
haazz <gkwldnjs121@gmail.com>
2024-03-30 15:32:19 +0900

    feat: root URL("/")에 Rest Docs 추가

--- backend/src/main/java/com/smusoak/restapi/controllers/RestDocsController.java
+++ backend/src/main/java/com/smusoak/restapi/controllers/RestDocsController.java
@@ -1,2 +1,15 @@
-package com.smusoak.restapi.controllers;public class RestDocsController {
+package com.smusoak.restapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class RestDocsController {
+    @GetMapping
+    public String hello() {
+        return "index";
+    }
 }
