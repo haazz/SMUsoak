137462deb4567f87afe286c09fd280aeffd70f09
OldJinGyu <chwlsrb00225@naver.com>
2024-04-14 21:11:27 +0900

    feat:채팅db 구축

---
+++ frontend/app/src/main/java/com/example/smu/ChatMessage.kt
@@ -0,0 +1,3 @@
+package com.example.smu
+
+data class ChatMessage(val sender: String, val message: String, val timestamp: String)
---
+++ frontend/app/src/main/java/com/example/smu/DatabaseChat.kt
@@ -0,0 +1,79 @@
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.smu.ChatMessage
+
+class DatabaseChat private constructor(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "chat_database.db"
+        private const val TABLE_NAME = "chat_messages"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_ROOM_ID = "room_id"
+        private const val COLUMN_SENDER = "sender"
+        private const val COLUMN_MESSAGE = "message"
+        private const val COLUMN_TIME = "time"
+
+        @Volatile
+        private var instance: DatabaseChat?= null
+
+        fun getInstance(context: Context)=
+            instance ?: synchronized(DatabaseChat::class.java){
+                instance ?:DatabaseChat(context).also{
+                    instance=it
+                }
+            }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        val createTableQuery = "CREATE TABLE $TABLE_NAME ($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_ROOM_ID INTEGER, $COLUMN_SENDER TEXT, $COLUMN_MESSAGE TEXT, $COLUMN_TIME TEXT)"
+        db.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        if(oldVersion != newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+            onCreate(db)
+        }
+    }
+
+    fun insertMessage(roomId: Int, sender: String, message: String, timestamp: String) {
+        val db = this.writableDatabase
+        val contentValues = ContentValues().apply{
+            put(COLUMN_ROOM_ID, roomId)
+            put(COLUMN_SENDER, sender)
+            put(COLUMN_MESSAGE, message)
+            put(COLUMN_TIME, timestamp)
+        }
+        db.insert(TABLE_NAME, null, contentValues)
+        db.close()
+    }
+
+    fun deleteChatroom(roomId: Int){
+        val db = this.writableDatabase
+        db.delete(TABLE_NAME, "$COLUMN_ID = $roomId", null)
+        db.close()
+    }
+
+    @SuppressLint("Range")
+    fun getAllMessages(roomId: Int): List<ChatMessage> {
+        val messages = ArrayList<ChatMessage>()
+        val db = readableDatabase
+        val cursor = db.query(TABLE_NAME, null, "$COLUMN_ROOM_ID= $roomId", null, null, null, COLUMN_TIME)
+
+        cursor.use {
+            while (it.moveToNext()) {
+                val sender = it.getString(it.getColumnIndex(COLUMN_SENDER))
+                val message = it.getString(it.getColumnIndex(COLUMN_MESSAGE))
+                val timestamp = it.getString(it.getColumnIndex(COLUMN_TIME))
+                val chatMessage = ChatMessage(sender, message, timestamp)
+                messages.add(chatMessage)
+            }
+        }
+        db.close()
+        return messages
+    }
+}
