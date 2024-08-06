b63ce710320e87c84569e40c37511db8d9007eeb
haazz <gkwldnjs121@gmail.com>
2024-08-06 10:25:14 +0900

    refactor: authentication

--- backend/src/main/java/com/smusoak/restapi/services/AuthenticationService.java
+++ backend/src/main/java/com/smusoak/restapi/services/AuthenticationService.java
@@ -10,6 +10,7 @@ import com.smusoak.restapi.repositories.UserRepository;
 import com.smusoak.restapi.response.CustomException;
 import com.smusoak.restapi.response.ErrorCode;
 import jakarta.mail.MessagingException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,7 +21,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.Random;
 
 @Service
@@ -36,21 +37,18 @@ public class AuthenticationService {
     private final RedisService redisService;
     private final MailService mailService;
     private static final int AUTH_CODE_INDEX = 0;
+    private static final int AUTH_CODE_LENGTH = 6;
 
     @Value("${spring.mail.auth-code-expirationms}")
     private long authCodeExpirationMillis;
 
     public JwtTokenDto.JwtAuthenticationResponse signin(UserDto.SigninRequest request) {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getMail(), request.getPassword()));
-        } catch (Exception e) {
-            throw new CustomException(ErrorCode.WRONG_MAIL_OR_PASSWORD);
-        }
-        var user = userRepository.findByMail(request.getMail())
-                .orElseThrow(() -> new CustomException(ErrorCode.WRONG_MAIL_OR_PASSWORD));
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getMail(), request.getPassword()));
+        User user = userRepository.findByMail(request.getMail())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
         // FCM token 업데이트
-        if(request.getFcmToken() != null && user.getFcmToken() != request.getFcmToken()) {
+        if (request.getFcmToken() != null && !user.getFcmToken().equals(request.getFcmToken())) {
             user.setFcmToken(request.getFcmToken());
             userRepository.save(user);
         }
@@ -58,73 +56,44 @@ public class AuthenticationService {
         return jwtService.generateToken(user);
     }
 
-    public JwtTokenDto.JwtAuthenticationResponse createUser(UserDto.SignupRequest request) {
-
-        // verifiedCode를 거치면 "true"가 redis에 저장되어 있어야 함
-        String auth = redisService.getListOpsByIndex(request.getMail(), AUTH_CODE_INDEX);
-        if (auth == null || auth.isEmpty()) {
-            throw new CustomException(ErrorCode.REDIS_DATA_NOT_FOUND);
-        }
-        else if (!auth.equals("true")) {
-            throw new CustomException(ErrorCode.WRONG_AUTH_CODE);
-        }
-        // 패스워드 규칙, 메일 중복, 닉네임 중복 검사
-        this.checkPasswordRule(request.getPassword());
-        this.checkDuplicatiedMail(request.getMail());
-        Optional<UserDetail> checkUserDetail = userDetailRepository.findByNickname(request.getNickname());
-        if (checkUserDetail.isPresent()) {
-            throw new CustomException(ErrorCode.USER_NICKNAME_DUPLICATE);
-        }
-
-        // UserDetail DB에 저장
-        UserDetail userDetail = UserDetail.builder()
-                .nickname(request.getNickname())
-                .gender(request.getGender())
-                .age(request.getAge())
-                .mbti(request.getMbti())
-                .build();
-        userDetailRepository.save(userDetail);
-        // 유저 DB에 저장
-        User user = User.builder()
-                .mail(request.getMail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.ROLE_USER)
-                .userDetail(userDetail)
-                .build();
-        userRepository.save(user);
+    @Transactional
+    public JwtTokenDto.JwtAuthenticationResponse signup(UserDto.SignupRequest request) {
+        validateSignup(request);
+        UserDetail userDetail = createUserDetail(request);
+        User user = createUser(request, userDetail);
         redisService.deleteByKey(request.getMail());
         // JWT Token 생성
         return jwtService.generateToken(user);
     }
 
     // 메일 인증 코드 전송
+    @Transactional
     public void sendCodeToMail(UserDto.SendCodeRequest request) throws MessagingException {
-        String toMail = request.getMail();
-        if (!toMail.endsWith("@sangmyung.kr")) {
+        if (!request.getMail().endsWith("@sangmyung.kr")) {
             throw new CustomException(ErrorCode.WRONG_MAIL_ADDRESS);
         }
-        this.checkDuplicatiedMail(toMail);
+        checkDuplicatiedMail(request.getMail());
         String title = "SMUsoak 이메일 인증 번호";
-        String authCode = this.createCode();
+        String authCode = createCode();
         String htmlContent = "<h1>SMUsoak 메일인증</h1>" +
                 "<br>SMUsoak에 오신것을 환영합니다!" +
                 "<br>아래 [인증 번호]를 앱으로 돌아가 입력해주세요." +
                 "<br><h1>" + authCode + "</h1>";
-        redisService.deleteByKey(toMail);
-        redisService.setListOps(toMail, authCode);
-        redisService.setExpire(toMail, authCodeExpirationMillis);
-        mailService.sendMail(toMail, title, htmlContent);
+        redisService.deleteByKey(request.getMail());
+        redisService.setListOps(request.getMail(), authCode);
+        redisService.setExpire(request.getMail(), authCodeExpirationMillis);
+        mailService.sendMail(request.getMail(), title, htmlContent);
     }
 
     // 메일 인증 코드 검증
+    @Transactional
     public void verifiedCode(UserDto.MailVerificationRequest request) {
-        this.checkDuplicatiedMail(request.getMail());
+        checkDuplicatiedMail(request.getMail());
         String redisAuthCode = redisService.getListOpsByIndex(request.getMail(), AUTH_CODE_INDEX);
 
-        if(redisAuthCode == null || redisAuthCode.isEmpty()) {
+        if (redisAuthCode == null || redisAuthCode.isEmpty()) {
             throw new CustomException(ErrorCode.REDIS_DATA_NOT_FOUND);
-        }
-        else if(!redisAuthCode.equals(request.getAuthCode()) && !redisAuthCode.equals("true")) {
+        } else if (!redisAuthCode.equals(request.getAuthCode()) && !redisAuthCode.equals("true")) {
             throw new CustomException(ErrorCode.WRONG_AUTH_CODE);
         }
         redisService.deleteByKey(request.getMail());
@@ -136,19 +105,50 @@ public class AuthenticationService {
         String username = jwtService.extractIssuer(refreshToken);
         String storedRefreshToken = redisService.getValues("/refreshToken/" + username);
         System.out.println("username: " + username + " refreshToken: " + storedRefreshToken);
-        if(storedRefreshToken == null || !storedRefreshToken.equals(refreshToken)) {
+        if (storedRefreshToken == null || !storedRefreshToken.equals(refreshToken)) {
             throw new CustomException(ErrorCode.JWT_TOKEN_INVALID);
         }
         return jwtService.generateToken(username);
     }
 
+    private void validateSignup(UserDto.SignupRequest request) {
+        // verifiedCode를 거치면 "true"가 redis에 저장되어 있어야 함
+        String auth = redisService.getListOpsByIndex(request.getMail(), AUTH_CODE_INDEX);
+        if (Objects.isNull(auth) || auth.isEmpty()) {
+            throw new CustomException(ErrorCode.REDIS_DATA_NOT_FOUND);
+        } else if (!auth.equals("true")) {
+            throw new CustomException(ErrorCode.WRONG_AUTH_CODE);
+        }
+        // 패스워드 규칙, 메일 중복, 닉네임 중복 검사
+        checkPasswordRule(request.getPassword());
+        checkDuplicatiedMail(request.getMail());
+        checkDuplicatiedNickname(request.getNickname());
+    }
+
+    private UserDetail createUserDetail(UserDto.SignupRequest request) {
+        return userDetailRepository.save(UserDetail.builder()
+                .nickname(request.getNickname())
+                .gender(request.getGender())
+                .age(request.getAge())
+                .mbti(request.getMbti())
+                .build());
+    }
+
+    private User createUser(UserDto.SignupRequest request, UserDetail userDetail) {
+        return userRepository.save(User.builder()
+                .mail(request.getMail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.ROLE_USER)
+                .userDetail(userDetail)
+                .build());
+    }
+
     // 메일 인증 코드 생성
     private String createCode() {
-        int length = 6;
         try {
             Random random = SecureRandom.getInstanceStrong();
             StringBuilder builder = new StringBuilder();
-            for(int i = 0; i < length; i++) {
+            for (int i = 0; i < AUTH_CODE_LENGTH; i++) {
                 builder.append(random.nextInt(10));
             }
             return builder.toString();
@@ -159,11 +159,19 @@ public class AuthenticationService {
     }
 
     private void checkDuplicatiedMail(String mail) {
-        Optional<User> users = userRepository.findByMail(mail);
-        if (users.isPresent()) {
-            log.debug("UserService.checkDuplicatedMail exception occur mail: " + mail);
-            throw new CustomException(ErrorCode.USER_MAIL_DUPLICATE);
-        }
+        userRepository.findByMail(mail)
+                .ifPresent(user -> {
+                    log.debug("UserService.checkDuplicatedMail exception occur mail: " + mail);
+                    throw new CustomException(ErrorCode.USER_MAIL_DUPLICATE);
+                });
+    }
+
+    private void checkDuplicatiedNickname(String nickname) {
+        userDetailRepository.findByNickname(nickname)
+                .ifPresent((userDetail) -> {
+                    log.debug("UserService.checkDuplicatedNickname exception occur nickname: " + nickname);
+                    throw new CustomException(ErrorCode.USER_NICKNAME_DUPLICATE);
+                });
     }
 
     private void checkPasswordRule(String password) {
